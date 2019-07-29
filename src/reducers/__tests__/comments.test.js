46b3254412da39153297a874e05aca87ec1cc331
Peter Williams <pedroeldiablo@yahoo.com>
2019-07-29 23:55:53 +0100

    comments reducer tests including handling unknow type

---
+++ src/reducers/__tests__/comments.test.js
@@ -0,0 +1,19 @@
+import commentsReducer from 'reducers/comments';
+import { SAVE_COMMENT } from 'actions/types';
+
+it('handles actions of the type SAVE_COMMMENT', () => {
+    const action = {
+        type: SAVE_COMMENT,
+        payload : 'New Comment'
+    };
+
+    const newState = commentsReducer([], action);
+
+    expect(newState).toEqual(['New Comment']);
+});
+
+it('handles action with unknown type', () => {
+    const newState = commentsReducer([], {});
+
+    expect(newState).toEqual([]);
+});
