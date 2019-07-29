9c7d4f843a69cd0c0556255cb434da095aada7f2
Peter Williams <pedroeldiablo@yahoo.com>
2019-07-29 23:34:14 +0100

    setting up redux store and root

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
+import Root from 'Root';
 
 let wrapped;
 
 beforeEach(() => {
-    wrapped = mount(<CommentBox />);
+    wrapped = mount(
+        <Root>
+            <CommentBox />
+        </Root>
+    );
 });
 
 afterEach(() => {
@@ -15,4 +20,28 @@ afterEach(() => {
 it('has a text area and a button', () => {   
    expect(wrapped.find('textarea').length).toEqual(1);
    expect(wrapped.find('button').length).toEqual(1);
+});
+
+describe('the text area', () => {
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change', {
+            target: { value: 'new comment' }
+        });
+        wrapped.update();
+    });
+
+    it('has a text area that users can write in', () => {   
+    
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+
+    });
+
+    it('has a text area that is emptied on submit', () => {   
+
+        wrapped.find('form').simulate('submit', {});
+        wrapped.update();
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+
+    });
+
 });
