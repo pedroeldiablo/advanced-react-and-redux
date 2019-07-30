bc2f71ba2b73aaaf42ddd0b48ab6c60f2e233483
Peter Williams <pedroeldiablo@yahoo.com>
2019-07-30 23:52:16 +0100

    adding tests for commentlist

---
+++ src/components/__tests__/CommentList.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentList from 'components/CommentList';
+import Root from 'Root';
+import { isTSAnyKeyword } from '@babel/types';
+
+
+let wrapped;
+
+beforeEach(() => {
+    const initialState = {
+        comments: ['Comment 1', 'Comment 2']
+    }
+
+    wrapped = mount(
+        <Root initialState={initialState}>
+            <CommentList />
+        </Root>
+    );
+});
+
+afterEach(() => {
+    wrapped.unmount();
+});
+
+it('creates one li per comment', () => {
+    expect(wrapped.find('li').length).toEqual(2);
+});
+
+it('shows the text for each comment', () => {
+    expect(wrapped.render().text()).toContain('Comment 1');
+    expect(wrapped.render().text()).toContain('Comment 2');
+});
