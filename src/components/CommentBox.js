c631f78fc49f7eba0eb777d8a7a47ab280183b2a
Peter Williams <pedroeldiablo@yahoo.com>
2019-08-05 00:56:18 +0100

    sets up logic for authentication routing using props.history.push

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -5,6 +5,23 @@ import * as actions from 'actions';
 class CommentBox extends Component {
     state = { comment: ''}
 
+    //Component just got rendered
+    componentDidMount() {
+        this.shouldNavigateAway();
+
+    }
+
+    //Component just got updated
+    componentDidUpdate() {
+        this.shouldNavigateAway();
+    }
+
+    shouldNavigateAway(){
+        if(!this.props.auth) {
+            this.props.history.push('/');
+        }
+    }
+
     handleChange = (event) => {
         this.setState({ comment: event.target.value});
     };
@@ -32,4 +49,8 @@ class CommentBox extends Component {
     }
 };
 
-export default connect(null, actions)(CommentBox);
+function mapStateToProps(state) {
+    return { auth: state.auth };
+}
+
+export default connect(mapStateToProps, actions)(CommentBox);
