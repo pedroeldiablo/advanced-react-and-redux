3a58d054b35d5e1f1a806ad2f2d0cdea0f9a67b9
Peter Williams <pedroeldiablo@yahoo.com>
2019-08-05 14:33:37 +0100

    asynchronous action creating middleware replacement for redux-promise

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -2,7 +2,17 @@ export default ({ dispatch }) => next => action => {
     //Check to see if the action has a promise on its 'payload' property
     //If it does, then wait for it to resolve
     //If it doesnt, then send the action on to the next middleware
+
     if(!action.payload || !action.payload.then) {
         return next(action);
     }
+    //we want to wait for the promise to resolve 
+    //(get its data) and then create a new action 
+    //with thta data and dispatch it
+    action.payload.then(function(response){
+        const newAction = { ...action, payload: response };
+        dispatch(newAction);
+
+    });
+
 };
